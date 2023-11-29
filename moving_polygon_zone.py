f41ab2a49dadcfeb9464fa656ea37f26f2126734
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2023-11-29 19:55:01 +0200

    transition to OOP

--- moving_polygon_zone.py
+++ moving_polygon_zone.py
@@ -46,110 +46,104 @@ with open(args.polygon, 'r') as file:
 # load the YOLOv8 model
 model = YOLO(args.weights)
 
-# open the video file
-video_path = args.source
 
-cap = cv2.VideoCapture(video_path)
-
-# build corner annotator
-corner_annotator = sv.BoxCornerAnnotator(thickness=2, corner_length=14)
-
-# set to False before first click assosication on frame
-start = False
+class MovingZoneAnnotator():
+    def __init__(self, model):
+        self.video_path = args.source
+        self.model = model
+        self.annotator = sv.BoxCornerAnnotator(thickness=2, corner_length=14)
+        self.start = False
+        self.cap = cv2.VideoCapture(self.video_path)
 
 
-def mouse_callback(event, x, y, flags, params)->None:
-    '''
-    Mouse callback function based on OpenCV functionalities
-    '''
-    # make global the point in order to access x,y coordinates
-    global point, start
+    def mouse_callback(self, event, x, y, flags, params)->None:
+        '''
+        Mouse callback function based on OpenCV functionalities
+        '''
+        # assign left-click down button as event
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.start = True
+            self.point = (x,y)
 
-    # assign left-click down button as event
-    if event == cv2.EVENT_LBUTTONDOWN:
-        start = True
-        point = (x,y)
+    def find_center_coordinates(self, points:np.ndarray)->tuple:
+        '''
+        Finds x,y coordinates of any given array of shape (N, >=3)
 
-def find_center_coordinates(points:np.ndarray)->tuple:
-    '''
-    Finds x,y coordinates of any given array of shape (N, >=3)
+        -Args:
+            points (np.ndarray): numpy array with shape (N,2) and N>=3
 
-    -Args:
-        points (np.ndarray): numpy array with shape (N,2) and N>=3
+        -Returns:
+            x_center (int): center coordinates of x-axis
+            y_center (int): center coordinates of y-axis
+        '''
+        x_center = int(np.mean(points[:, 0]))
+        y_center = int(np.mean(points[:, 1]))
+        return x_center, y_center
 
-    -Returns:
-        x_center (int): center coordinates of x-axis
-        y_center (int): center coordinates of y-axis
-    '''
-    x_center = int(np.mean(points[:, 0]))
-    y_center = int(np.mean(points[:, 1]))
-    return x_center, y_center
+    def main(self):
+        # loop through the video frames
+        while self.cap.isOpened():
+            # read a frame from the video
+            success, frame = self.cap.read()
 
+            if success:
 
-def main():
-    # loop through the video frames
-    while cap.isOpened():
-        # read a frame from the video
-        success, frame = cap.read()
+                # run YOLOv8 inference on the frame
+                results = model(frame)
 
-        if success:
+                # annotator zone starts after the first click on frame
+                if self.start:
+                    # unpacking of x,y coordinates
+                    x,y = self.point
 
-            # run YOLOv8 inference on the frame
-            results = model(frame)
+                    # initialize an empty NumPy array and fill the points
+                    points = np.empty((0, 2), dtype=int)
+                    for i in polygone.values():
+                        points = np.append(points, [i], axis=0)
 
-            # annotator zone starts after the first click on frame
-            if start:
-                # unpacking of x,y coordinates
-                x,y = point
+                    x_center, y_center = self.find_center_coordinates(points=points)
+                    
+                    points = points + np.array([x-x_center, y-y_center])
 
-                # initialize an empty NumPy array and fill the points
-                points = np.empty((0, 2), dtype=int)
-                for i in polygone.values():
-                    points = np.append(points, [i], axis=0)
+                    # building polygon zone and annotator
+                    zone = sv.PolygonZone(polygon=points,
+                                        frame_resolution_wh=(frame.shape[1], frame.shape[0]),
+                                        triggering_position=sv.Position.CENTER)
+                    zone_annotator = sv.PolygonZoneAnnotator(zone=zone,
+                                                            color=sv.Color.red())
 
-                x_center, y_center = find_center_coordinates(points=points)
-                
-                points = points + np.array([x-x_center, y-y_center])
+                    # pass results to Detections Class and activate zone triggering
+                    detections = sv.Detections.from_ultralytics(results[0])
+                    mask = zone.trigger(detections=detections)
+                    
+                    # filter detections in polygon zone
+                    detections = detections[mask]
 
-                # building polygon zone and annotator
-                zone = sv.PolygonZone(polygon=points,
-                                    frame_resolution_wh=(frame.shape[1], frame.shape[0]),
-                                    triggering_position=sv.Position.CENTER)
-                zone_annotator = sv.PolygonZoneAnnotator(zone=zone,
-                                                         color=sv.Color.red())
+                    # visualize the results on the frame
+                    frame = self.annotator.annotate(scene=frame.copy(),
+                                                    detections=detections)
 
-                # pass results to Detections Class and activate zone triggering
-                detections = sv.Detections.from_ultralytics(results[0])
-                mask = zone.trigger(detections=detections)
-                
-                # filter detections in polygon zone
-                detections = detections[mask]
-
-                # visualize the results on the frame
-                frame = corner_annotator.annotate(scene=frame.copy(),
-                                                  detections=detections)
-
-                # plot zone annotator
-                zone_annotator.annotate(frame)
+                    # plot zone annotator
+                    zone_annotator.annotate(frame)
 
 
-            # display the annotated frame
-            cv2.namedWindow("Moving polygon zone")
-            cv2.setMouseCallback("Moving polygon zone", mouse_callback)
-            cv2.imshow("Moving polygon zone", frame)
+                # display the annotated frame
+                cv2.namedWindow("Moving polygon zone")
+                cv2.setMouseCallback("Moving polygon zone", self.mouse_callback)
+                cv2.imshow("Moving polygon zone", frame)
 
 
-            # break the loop if 'q' is pressed
-            if cv2.waitKey(1) & 0xFF == ord("q"):
+                # break the loop if 'q' is pressed
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+            else:
+                # break the loop if the end of the video is reached
                 break
-        else:
-            # break the loop if the end of the video is reached
-            break
 
-    # release the video capture object and close the display window
-    cap.release()
-    cv2.destroyAllWindows()
+        # release the video capture object and close the display window
+        self.cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    main()
+    MovingZoneAnnotator(model=model).main()
